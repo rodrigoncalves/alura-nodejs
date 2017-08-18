00060352bf7326227b7b55a7ea0520cd744ddbaa
Rodrigo Gonçalves <rgoncalves@esec.com.br>
2017-08-18 12:53:39 -0300

    2.5

--- app/routes/produtos.js
+++ app/routes/produtos.js
@@ -1,12 +1,9 @@
+var dbConnection = require('../infra/connectionFactory');
+
 module.exports = function(app) {
     app.get('/produtos', function(req, res) {
-        var mysql = require('mysql');
-        var connection = mysql.createConnection({
-            host: 'localhost',
-            user: 'root',
-            password: '',
-            database: 'casadocodigo_nodejs'
-        });
+
+        var connection = dbConnection();
 
         connection.query('select * from livros', function(err, results) {
             res.render('produtos/lista', {lista:results});
