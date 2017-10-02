568cf72333edd1839631e48c168a91681dffed2e
Rodrigo Gonçalves <rodrigosg2000@gmail.com>
2017-10-02 12:30:11 -0300

    7.8

--- test/produtos.js
+++ test/produtos.js
@@ -2,6 +2,14 @@ var express = require('../config/express')();
 var request = require('supertest')(express);
 
 describe('ProdutosController', function() {
+
+    beforeEach(function(done) {
+        var conn = express.infra.connectionFactory();
+        conn.query("delete from produtos", function(err, result) {
+            if (!err) done();
+        });
+    });
+
     it('#listagem json', function(done) {
         request.get('/produtos')
             .set('Accept', 'application/json')
