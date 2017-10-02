2352cc6345dea8d0523ccff601d5cf5c8fbead39
Rodrigo Gonçalves <rodrigosg2000@gmail.com>
2017-10-02 11:59:25 -0300

    7.2

--- test/produtos.js
+++ test/produtos.js
@@ -1,5 +1,26 @@
+var http = require('http');
+
 describe('ProdutosController', function() {
-    it('#listagem json', function() {
-        console.log('teste de verificação de listagem de json');
+    it('#listagem json', function(done) {
+        var configuracoes = {
+            hostname: 'localhost',
+            port:3000,
+            path:'/produtos',
+            headers: {
+                'Accept' : 'application/json'
+            }
+        };
+
+        http.get(configuracoes, function(res){
+            if (res.statusCode == 200) {
+                console.log('Status OK');
+            }
+
+            if (res.headers['content-type'] == 'application/json; charset=utf-8') {
+                console.log('Content-type OK');
+            }
+
+            done();
+        });
     });
 });
